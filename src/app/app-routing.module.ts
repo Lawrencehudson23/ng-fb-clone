f41765824891e9aa6d3edc2ea3a4fd46e78181f3
shijieliu <lawrencehudson23@gmail.com>
2020-12-29 21:23:23 -0800

    build: init commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,30 @@
+import { HomeComponent } from './components/home/home.component';
+import { FacebookGuard } from './guards/facebook.guard';
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    // loadChildren: () =>
+    // import('./components/home/home.module').then((m) => m.HomeModule),
+    canActivate: [FacebookGuard],
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
